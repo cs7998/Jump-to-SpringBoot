f2f7eb97480796574c07e408a7d7663abfd73da0
unho.shin <cs7998@gmail.com>
2023-02-26 23:19:24 +0900

    [Jump to SpringBoot][02][08] Root URL redirect

--- src/main/java/com/uhshin/sbb/MainController.java
+++ src/main/java/com/uhshin/sbb/MainController.java
@@ -12,6 +12,14 @@ public class MainController {
 	public String index() {
 		return "안녕하세요 sbb에 오심을 환영하오~";
 	}
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";
+	}
+	// redirect:<URL> URL 로 리다이렉트 - 완전히 새로운 URL 로 요청된다.
+	// forward:<URL> URL로 포워드 - 기존 요청값이 유지된 상태로 URL이 전환된다.
+	
 //	public void index() {
 //		System.out.println("index");
 //	}  @ResponseBody 없이는 500 Internal Error 발생함
