ac86bbbc4dfb72c0485c1fa78b5dc2228c6239f8
unho.shin <cs7998@gmail.com>
2023-02-26 23:49:00 +0900

    [Jump to SpringBoot][02][09] Service

--- src/main/java/com/uhshin/sbb/question/QuestionController.java
+++ src/main/java/com/uhshin/sbb/question/QuestionController.java
@@ -13,11 +13,13 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class QuestionController {
 
-	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
+	//private final QuestionRepository questionRepository;
 	
 	@GetMapping("/question/list")
 	public String list(Model model){
-		List<Question> questionList = this.questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
+		//List<Question> questionList = this.questionRepository.findAll();
 		// 실제 프로젝트에서 이렇게 하면 안될듯 한다.. 질문 개수가 많아지면 당연히 성능에 문제가 생기겠지..
 		model.addAttribute("questionList", questionList);
 		return "question_list";
