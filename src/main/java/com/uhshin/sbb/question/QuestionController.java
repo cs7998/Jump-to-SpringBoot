7310606a0bee3f68733a8a5872c514610820185e
unho.shin <cs7998@gmail.com>
2023-03-05 20:37:23 +0900

    [Jump to SpringBoot][02][15] springboot form validation

--- src/main/java/com/uhshin/sbb/question/QuestionController.java
+++ src/main/java/com/uhshin/sbb/question/QuestionController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -45,14 +47,18 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/create") // GET 으로 질문 양식 가져오기
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
-	@PostMapping("/create") // POST 로 질문 저장하기
-	public String questionCreate(@RequestParam  String subject, @RequestParam String content) {
+	// POST 로 질문 저장하기
+	@PostMapping("/create") 
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
 		// TODO 질문을 저장한다.
-		this.questionService.create(subject, content);
+		if(bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list";	// 질문 저장후 질문 목록으로 이동한다.
 	}
 }
