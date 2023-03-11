8024672e01889871df8741c1aea11fedaaa7a627
unho.shin <cs7998@gmail.com>
2023-03-11 22:09:31 +0900

    [Jump to SpringBoot][03][07] Logout

--- src/main/java/com/uhshin/sbb/SecurityConfig.java
+++ src/main/java/com/uhshin/sbb/SecurityConfig.java
@@ -32,6 +32,11 @@ public class SecurityConfig {
 			.formLogin()
 			.loginPage("/user/login")
 			.defaultSuccessUrl("/")
+		.and()
+			.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+			.logoutSuccessUrl("/")
+			.invalidateHttpSession(true)
 		;
 		return http.build();
 	}
