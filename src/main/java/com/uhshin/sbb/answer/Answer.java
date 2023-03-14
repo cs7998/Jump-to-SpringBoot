c14acf8acee75efd14c272facec6c9d6e174d02e
unho.shin <cs7998@gmail.com>
2023-03-14 22:50:35 +0900

    [Jump to SpringBoot][03][11] change Entity Question, Answer - voter

--- src/main/java/com/uhshin/sbb/answer/Answer.java
+++ src/main/java/com/uhshin/sbb/answer/Answer.java
@@ -1,6 +1,7 @@
 package com.uhshin.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.uhshin.sbb.question.Question;
 import com.uhshin.sbb.user.SiteUser;
@@ -10,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -38,4 +40,7 @@ public class Answer {
 	private SiteUser author;
 
 	private LocalDateTime modifyDate;
+
+	@ManyToMany
+	Set<SiteUser> voter;
 }
--- src/main/java/com/uhshin/sbb/question/Question.java
+++ src/main/java/com/uhshin/sbb/question/Question.java
@@ -2,6 +2,7 @@ package com.uhshin.sbb.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.uhshin.sbb.answer.Answer;
 import com.uhshin.sbb.user.SiteUser;
@@ -12,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -44,4 +46,7 @@ public class Question {
 	private SiteUser author;
 
 	private LocalDateTime modifyDate;
+
+	@ManyToMany
+	Set<SiteUser> voter;  // Set 중복 허용하지 않음, 중복으로 추천하면 안된다.
 }
