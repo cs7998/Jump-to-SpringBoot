524f3fa832878f734a9dfc946a937041778ad784
unho.shin <cs7998@gmail.com>
2023-03-14 21:54:07 +0900

    [Jump to SpringBoot][03][10] modify Answer

--- src/main/java/com/uhshin/sbb/answer/AnswerService.java
+++ src/main/java/com/uhshin/sbb/answer/AnswerService.java
@@ -1,13 +1,15 @@
 package com.uhshin.sbb.answer;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.uhshin.sbb.DataNotFoundException;
 import com.uhshin.sbb.question.Question;
 import com.uhshin.sbb.user.SiteUser;
 
 import lombok.RequiredArgsConstructor;
-
-import java.time.LocalDateTime;
-
-import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +23,21 @@ public class AnswerService {
 		answer.setCreateDate(LocalDateTime.now()); 
 		answer.setQuestion(question);
 		answer.setAuthor(author);
+		this.answerRepository.save(answer);
+	}
+
+	public Answer getAnswer(Integer id) {
+		Optional<Answer> answer = this.answerRepository.findById(id);
+		if(answer.isPresent()) {
+			return answer.get();
+		} else {
+			throw new DataNotFoundException("답변을 찾지 못했습니다.");
+		}
+	}
+
+	public void modify(Answer answer, String content) {
+		answer.setContent(content);
+		answer.setModifyDate(LocalDateTime.now());
 		this.answerRepository.save(answer);
 	}
 }
